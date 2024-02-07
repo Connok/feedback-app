61eeda7551927ee2cf712543b822d9ee1c0755d6
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-02-07 00:01:18 -0500

    desplayed json.db data on frontend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,52 +1,30 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4} from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedBack] = useState([
-        {
-            id:1,
-            text: 'This is feedback item 1',
-            rating: 4,
-
-        },
-        {
-            id:2,
-            text: 'This is feedback item 2',
-            rating: 2,
-
-        },
-        {
-            id:3,
-            text: 'This is feedback item 3',
-            rating: 8,
-
-        },
-        {
-            id:4,
-            text: 'This is feedback item 4',
-            rating: 9,
-
-        },
-        {
-            id:5,
-            text: 'This is feedback item 5',
-            rating: 1,
-
-        },
-        {
-            id:6,
-            text: 'This is feedback item 6',
-            rating: 7,
-
-        },
-    ])
+    const [isLoading, setIsLoading] = useState(true)
+    const [feedback, setFeedBack] = useState([])
 
     const [feedbackEdit, setFeedBackEdit] = useState({
         item: {},
         edit: false,
     })
+
+    useEffect(() => {
+        fetchFeedback();
+    }, [])
+
+    // fetch feedback
+const fetchFeedback = async () => {
+    const response = await fetch(`http://localhost:5000/feedback?_sort=id`)
+    const data = await response.json()
+
+    setFeedBack(data)
+    setIsLoading(false)
+}
+
 // delete feedback item
     const deleteFeedback = (id) => {
         if(window.confirm("Are you sure you want to delete?")){
@@ -77,10 +55,12 @@ export const FeedbackProvider = ({children}) => {
 
     return <FeedbackContext.Provider value={{
         feedback,
+        feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
-        feedbackEdit,
+
         updateFeedback,
     }}>
         {children}
